b41fadae0e0a88c8049d61977bd7c4983efd66f0
NamProPlayer <nguyenthitrangg01011976@gmail.com>
2025-02-09 23:08:14 +0700

    access control

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv/config");
-
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 const app = express();
 
 // Middleware for JSON parsing
